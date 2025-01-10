7d05a3c564bfeff9487631f5a76118e6367fb6f8
Iris <iris.devillars@gmail.com>
2025-01-10 09:07:05 +0100

    fix: error messages in process_tx

--- src/server/process_tx.rs
+++ src/server/process_tx.rs
@@ -143,6 +143,13 @@ async fn process_tx(
                             "Failed to process deposit transaction for tx_id: {}: {:?}",
                             tx.location.tx_id, e
                         ));
+                        return Err(e);
+                    } else {
+                        state.logger.info(format!(
+                            "Processed deposit transaction for tx_id: {}",
+                            tx.location.tx_id
+                        ));
+                        return Ok(());
                     }
                 }
             }
@@ -156,5 +163,5 @@ async fn process_tx(
         }
     }
 
-    Ok(())
+    Err(anyhow::anyhow!("Failed to process transaction. Unable to find a matching deposit."))
 }
